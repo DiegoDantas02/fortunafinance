a3bbf6d0938206b16dd3d4f99210802572d27d1a
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-09 11:38:55 -0300

    .

--- inc/header.php
+++ inc/header.php
@@ -14,8 +14,8 @@
     <header>
         <a href="../inicio/home.php"><img src="../img/logo.png" alt="Logo"></a>
         <div class="buttons">
-            <a href="../inicio/home.php">Início</a>
-            <a href="../tela-bloqueio/login.php">Sair</a>
+            <a href="../inicio/home.php" style="background-color:green; border-radius: 2px; font-size:20px;">Início</a>
+            <a href="../tela-bloqueio/login.php" style="border-radius: 2px; font-size:20px;">Sair</a>
         </div>
     </header>
     <div style="margin-top: 70px;"> <!-- Espaço para evitar que o conteúdo seja sobreposto pelo cabeçalho -->
