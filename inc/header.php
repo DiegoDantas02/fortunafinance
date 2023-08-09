220f9b5e15e4f6c93852a5cf512a4552af66af8e
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-09 16:04:38 -0300

    .

--- inc/header.php
+++ inc/header.php
@@ -4,23 +4,20 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./style.css" />
-    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../inicio/style.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Controle de despesas</title>
-    
+   
 </head>
 
 <body>
     <header>
         <a href="../inicio/home.php"><img src="../img/logo.png" alt="Logo"></a>
         <div class="buttons">
-            <a href="../inicio/home.php" style="background-color:green; border-radius: 2px; font-size:20px;">Início</a>
-            <a href="../tela-bloqueio/login.php" style="border-radius: 2px; font-size:20px;">Sair</a>
+            <a href="../inicio/home.php">Ínicio</a>
+            <a href="../tela-bloqueio/login.php">Sair</a>
         </div>
     </header>
-    <div style="margin-top: 70px;"> <!-- Espaço para evitar que o conteúdo seja sobreposto pelo cabeçalho -->
-        <!-- Seu conteúdo aqui -->
-    </div>
 </body>
 
 </html>
--- test/footer.php
+++
@@ -1,5 +0,0 @@
-<footer >
-    <p> &copy; 2023 Software De Finanças - Desenvolvido por 🤍 <a href="mailto:diegorodriguesdantas02@gmail.com" style="text-decoration: none; color: green;">Diego Dantas</a></p>
-</footer>
-</body>
-</html>
