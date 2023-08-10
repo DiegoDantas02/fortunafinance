c60a6f90874fa6f332c871971cfe33a3e8a0c3f8
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-10 13:41:44 -0300

    .

--- inc/header.php
+++ inc/header.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./style.css" />
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
     <title>Controle de despesas</title>
     
 </head>
 
 <body>
     <header>
-        <a href="../inicio/home.php"><img src="../img/logo.png" alt="Logo" style="height: 55px;"></a>
+        <a href="../inicio/home.php"><img src="../img/logo.png" alt="Logo" style="height: 55px; padding-left:20px; width:200px;"></a>
         <div class="buttons">
             <a href="../inicio/home.php"style="background-color:green; border-radius: 5px; font-size:20px;"; >Início</a>
             <a href="../tela-bloqueio/login.php" style="border-radius: 5px; size:30px; font-size:20px;">Sair</a>
