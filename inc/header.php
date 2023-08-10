48de0994404c84ebd189eb7801edd9a8a7b57ac7
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-10 16:24:29 -0300

    .

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../inicio/style.css" />
+    <!-- <link rel="stylesheet" href="../inicio/style.css" /> -->
     <link rel="stylesheet" href="../css/header.css">
     <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
     
