955453910e48cc68fe8b83738320c9aacd0448f5
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-10 13:52:22 -0300

    arrumando os css do header

--- inc/header.php
+++ inc/header.php
@@ -6,21 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./style.css" />
     <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
-    <title>Controle de despesas</title>
     
+    <title>Controle de despesas</title>
 </head>
-
 <body>
     <header>
-        <a href="../inicio/home.php"><img src="../img/logo.png" alt="Logo" style="height: 55px; padding-left:20px; width:200px;"></a>
+        <a href="../inicio/home.php"><img src="../img/logo.png" alt="Logo" class="logo"></a>
         <div class="buttons">
-            <a href="../inicio/home.php"style="background-color:green; border-radius: 5px; font-size:20px;"; >Início</a>
-            <a href="../tela-bloqueio/login.php" style="border-radius: 5px; size:30px; font-size:20px;">Sair</a>
+            <a href="../inicio/home.php" class="botao-inicio-header">Início</a>
+            <a href="../tela-bloqueio/login.php" class="botao-sair-header">Sair</a>
         </div>
     </header>
     <div style="margin-top: 70px;"> <!-- Espaço para evitar que o conteúdo seja sobreposto pelo cabeçalho -->
-        <!-- Seu conteúdo aqui -->
+
     </div>
 </body>
-
 </html>
