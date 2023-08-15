1eebeb54358da739062932206f710ea501f956a0
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-15 16:30:49 -0300

    .

--- menu/indexsidebar.php
+++ menu/indexsidebar.php
@@ -4,17 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../menu/sidebarstyle.css"> 
-    <title> Menu Responsivo</title>
+    <link rel="stylesheet" href="../menu/sidebarstyle.css">
+    <!-- Certifique-se de incluir o link para a fonte do Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <title>Menu Responsivo</title>
 </head>
-<?php include "../inc/conexao.php";?>
+<?php include "../inc/conexao.php"; ?>
 
 <body>
     <input type="checkbox" id="check">
-    
 
     <header>
-        <!-- header começo -->
+        <!-- Header começo -->
         <label for="check">
             <ion-icon name="menu-outline" id="sidebar_btn"></ion-icon>
         </label>
@@ -25,26 +26,22 @@
             <a href="../tela-bloqueio/login.php" class="sair_btn">Sair</a>
         </div>
     </header>
-    <!-- header final -->
-    
+    <!-- Header final -->
+
     <!-- Sidebar começo  -->
     <div class="sidebar">
         <center>
             <img src="../img/icone.png" alt="" class="image">
-            
-            <h2>Usuário $Fortuna </h2>
+            <h2>Usuário $Fortuna</h2>
         </center>
-        <a href="#"><ion-icon name="images-outline"></ion-icon><span>Inicio</span></a>
-        <a href="../inicio//home.php"><ion-icon name="desktop-outline"></ion-icon><span>Painel</span></a>
-        <!-- <a href="#"><ion-icon name="calendar-clear-outline"></ion-icon><span>Galeria</span></a> -->
-        <a href="../sobrenos/sobre.php"> <ion-icon name="alert-circle-outline"></ion-icon><span>Sobre</span></a>
-        <!-- <a href="#"><ion-icon name="settings-outline"></ion-icon><span>Configuração</span></a> -->
+        <a href="../menu/indexsidebar.php"><ion-icon name="home-outline" class="icon"></ion-icon><span>Início</span></a>
+        <a href="../inicio/home.php"><ion-icon name="stats-chart-outline" class="icon"></ion-icon><span>Painel</span></a>
+        <a href="../sobrenos/sobre.php"><ion-icon name="help-circle-outline" class="icon"></ion-icon><span>Sobre</span></a>
     </div>
-    
     <!-- Sidebar final  -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
     <div class="content"></div>
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
