22aa1268c8c85b56921128daa1c02ea8ea93702a
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-16 10:11:48 -0300

    .

--- menu/indexsidebar.php
+++ menu/indexsidebar.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../menu/sidebarstyle.css">
     <!-- Certifique-se de incluir o link para a fonte do Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    
     <title>Menu Responsivo</title>
 </head>
 <?php include "../inc/conexao.php"; ?>
