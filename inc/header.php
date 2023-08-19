4bc9f9cb15b88a1ffac83d84dee4cd0b95e19642
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-08-19 13:27:34 -0300

    arrumando a bagunça

--- inc/header.php
+++ inc/header.php
@@ -14,16 +14,22 @@
         }
 
         header {
-            background-color: black; /* Fundo preto */
-            color: white; /* Texto branco */
+            background-color: black;
+            /* Fundo preto */
+            color: white;
+            /* Texto branco */
             padding: 5px;
             width: 1800px;
             display: flex;
             justify-content: space-between;
-            position: fixed; /* Torna o cabeçalho fixo */
-            top: 0; /* Fixa no topo da página */
-            width: 100%; /* Faz o cabeçalho ocupar toda a largura */
-            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Box shadow com deslocamento X, deslocamento Y, raio e cor */
+            position: fixed;
+            /* Torna o cabeçalho fixo */
+            top: 0;
+            /* Fixa no topo da página */
+            width: 100%;
+            /* Faz o cabeçalho ocupar toda a largura */
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+            /* Box shadow com deslocamento X, deslocamento Y, raio e cor */
         }
 
         .logo {
@@ -32,7 +38,7 @@
         }
 
         .buttons {
-            margin-top: 20px;
+            margin-top: -9px;
             gap: 10px;
         }
 
@@ -43,16 +49,17 @@
             font-size: 20px;
             color: #fff;
         }
-        
+
         /* Cor de fundo botão "Início" */
         .buttons a:nth-child(1) {
             background-color: green;
         }
-        
+
         /* Cor de fundo botão "Sair" */
         .buttons a:nth-child(2) {
             background-color: #555;
-            transition: 0.3s; /* Adicionado para criar um efeito de destaque no botão "Início" */
+            transition: 0.3s;
+            /* Adicionado para criar um efeito de destaque no botão "Início" */
         }
 
         /* Cor de fundo botão "Sair" no hover */
@@ -74,14 +81,43 @@
                 padding: 5px;
             }
         }
+/* headerrrrrrrrrrrrrrrrrrrrr */
+        header {
+            position: fixed;
+            background: #073816;
+            padding: 30px;
+            width: 100%;
+            height: 30px;
+            box-shadow: 0px 4px 10px rgba(0, 255, 4, 0.3);
+            
+
+        }
+
+        .left h3 {
+            color: #eaff00;
+            margin: 0;
+            text-transform: uppercase;
+            font-size: 25px;
+            font-weight: 800;
+            margin-top: -11.3px;
+            text-decoration: none;
+        }
+
+        .left span {
+            color: #ffffff;
+            text-decoration: none;
+        }
     </style>
 </head>
 
 <body>
     <header>
-        <a href="../inicio/home.php">
-            <img src="../img/logo.png" alt="Logo" class="logo">
-        </a>
+            <!-- <img src="../img/logo.png" alt="Logo" class="logo"> -->
+            <div class="left" style="text-decoration: none;">
+                <a  href="../inicio/home.php" style="text-decoration: none;"><h3>$Fortuna <span>Finance</span></h3></a>
+    
+            </div>
+
         <div class="buttons">
             <a href="../menu/indexsidebar.php">Início</a>
             <a href="../tela-bloqueio/login.php">Sair</a>
